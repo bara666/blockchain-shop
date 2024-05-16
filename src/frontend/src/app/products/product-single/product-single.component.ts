54a32a19cb1354d045e4f41189067e6d23804025
Manuel Barallobre <manuel.barallobre@disashop.com>
2024-05-16 19:34:21 +0000

    feat(frontend): add pokemon products and vendors

--- src/frontend/src/app/products/product-single/product-single.component.ts
+++ src/frontend/src/app/products/product-single/product-single.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-single',
   templateUrl: './product-single.component.html',
-  styleUrl: './product-single.component.scss'
+  styleUrl: './product-single.component.scss',
 })
-export class ProductSingleComponent {
+export class ProductSingleComponent implements OnChanges {
 
   @Input() product: any;
+  public productComplet: any;
 
+  constructor(private productosService: ProductsService) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['product'] && changes['product'].currentValue) {
+      this.productosService.getProductByName(this.product.name).then(product => {
+        this.productComplet = product;
+      });
+    }
+  }
 }
--- src/frontend/src/app/products/products-list/products-list.component.ts
+++ src/frontend/src/app/products/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+
 
 @Component({
   selector: 'app-products-list',
@@ -6,9 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './products-list.component.scss'
 })
 export class ProductsListComponent {
-  gridColumns = 3;
+  gridColumns = 5;
+  products: any = [];
+  productsCount = 0;
+  page = 0;
+  limit = 20;
+
+  constructor(private productosService: ProductsService) {
+    this.productosService.productsCount$.subscribe(count => {
+      this.productsCount = count;
+    });
+  }
+
+  ngOnInit() {
+    this.cargaProductos();
+  }
+
+  async cargaProductos(page: number | null = null) {
+    if (page == null) {
+      page = this.page;
+    }
+    await this.productosService.getProductsList(page * this.limit, this.limit).then(products => {
+      this.products = products;
+    });
+  }
 
   toggleGridColumns() {
     this.gridColumns = this.gridColumns === 3 ? 4 : 3;
+  }
+
+  async cambiarPagina(event: any) {
+    this.page = event.pageIndex;
+    this.limit = event.pageSize;
+    await this.cargaProductos();
   }
 }
