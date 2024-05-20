9d238be8c1e73e5ea292e16b77ec0de4c09df779
Eduardo Alonso <eduardo.alonso@disashop.com>
2024-05-20 02:35:41 +0200

    fix(backend): typo in path to dockerfile

--- deployment/cluster.sh
+++ deployment/cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kind create cluster --name blockchain-cluster --config kind-config.yaml
-docker build -t blockchain-backend:0.1 -f ../Dockerfile ../
+docker build -t blockchain-backend:0.1 -f ../src/backend/Dockerfile ../src/backend/
 kind load --name blockchain-cluster docker-image blockchain-backend:0.1
 kubectl apply -f start-blockchain-gen.yml
 kubectl apply -f start-blockchain-node.yml
